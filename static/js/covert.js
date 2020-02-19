375e40d0d86469705f1dcd5b7c8a5c79d55af563
Ryan <ryan@ryanwans.com>
2020-02-18 21:48:08 -0500

    imporved header

--- static/js/covert.js
+++ static/js/covert.js
@@ -2,5 +2,8 @@
 
 ~(function() {
     let _eventQue = [];
-    
+    (document.title !== 'Second Chance')
+        ? document.title = "Second Chance"
+        : null;
+    console['log']("[RWAPI] Covert.js Script Loaded and Running!\nWebsite by Ryan Wans c/o Nathan Aldridge");
 })();
